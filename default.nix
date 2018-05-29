494cad088998e4db9f9f622b6e6946eaf5974d0b
S. M. Hickman <sean@grump.ly>
2018-05-29 00:30:28 -0500

    Initial commit.

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ mkDerivation, base, containers, ef, pure-core, pure-default, pure-txt, template-haskell, stdenv
+, useTemplateHaskell ? true
+}:
+mkDerivation {
+  pname = "pure-css";
+  version = "0.7.0.0";
+  src = ./.;
+  libraryHaskellDepends = [ base containers ef pure-core pure-default pure-txt ] ++ (if useTemplateHaskell then [ template-haskell ] else []);
+  configureFlags = (if useTemplateHaskell then [ ] else [ "-f-use-template-haskell" ]);
+  homepage = "github.com/grumply/pure-css";
+  license = stdenv.lib.licenses.bsd3;
+}
