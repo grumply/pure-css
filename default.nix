1cf7b405cad9a86c452264be46a39c730e20af88
Sean Hickman <sean@grumply.com>
2020-02-13 23:30:08 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "pure-css";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base containers ef pure-core pure-default pure-txt ] ++ (if noUseTemplateHaskell then [ ] else [ template-haskell ] );
   configureFlags = (if noUseTemplateHaskell then [ ] else [ "-f-use-template-haskell" ] );
