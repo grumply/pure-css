a0d2632b7e9a77d0a71177943338f7a5a10f40b7
Sean Hickman <sean@grumply.com>
2020-10-03 03:24:11 -0500

    Merge style and selector DSLs to eliminate apply scoping.

--- src/Pure/Data/CSS.hs
+++ src/Pure/Data/CSS.hs
@@ -2,148 +2,148 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Pure.Data.CSS where
 
--- from ef
-import Ef
+import Pure.Data.View ( View )
+import Pure.Data.View.Patterns ( txt )
+import Ef ( send, Narrative(..) )
+import Pure.Data.Txt as Txt ( intercalate, replicate, null, toTxt, Txt )
+import Data.Functor.Identity ( Identity(runIdentity) )
+import Control.Monad ( void )
+import Data.Traversable (for)
 
--- from pure-core
-import Pure.Data.View
-import Pure.Data.View.Patterns
+data CSS_ k where
+  Raw_    :: Txt -> k -> CSS_ k
+  Scope_  :: (Txt -> k) -> CSS_ k
+  Selection_ :: Txt -> CSS a -> (a -> k) -> CSS_ k
+  Wrap_   :: Txt -> CSS a -> (a -> k) -> CSS_ k
+  Style_  :: Txt -> Txt -> k -> CSS_ k
 
--- from pure-default
-import Pure.Data.Default
+instance Functor CSS_ where
+  fmap f (Raw_ r k) = Raw_ r (f k)
+  fmap f (Scope_ g) = Scope_ (f . g)
+  fmap f (Selection_ sel scoped ak) = Selection_ sel scoped (f . ak)
+  fmap f (Wrap_ rule scoped ak) = Wrap_ rule scoped (f . ak)
+  fmap f (Style_ k v a) = Style_ k v (f a)
 
--- from pure-txt
-import Pure.Data.Txt as Txt
+type CSS = Narrative CSS_ Identity
 
--- from base
-import Data.Char
-import Control.Category as C
-import Data.Bifunctor
-import Data.Foldable as F
-import Data.Functor.Identity
-import qualified Data.List as List
-import Data.Monoid
-import Data.String
-import Data.Traversable
-import Unsafe.Coerce
-import Prelude hiding ((.),id)
+-- * Commands
 
--- from containers
-import qualified Data.Map.Strict as M
+scope :: CSS Txt
+scope = send (Scope_ id)
 
--- from template-haskell
-import qualified Language.Haskell.TH.Syntax as TH
-
-import Data.String (IsString(..))
-
-data Styles_ k where
-  Style_ :: Txt -> Txt -> k -> Styles_ k
-  deriving Functor
-
-type Styles = Narrative Styles_ Identity
+rawCSS :: Txt -> CSS ()
+rawCSS r = send (Raw_ r ())
 
 infixr 0 =:
-(=:) :: Txt -> Txt -> Styles Txt
+(=:) :: Txt -> Txt -> CSS Txt
 (=:) nm val = send (Style_ nm val val)
 
 infixr 0 =*
-(=*) :: Txt -> [Txt] -> Styles Txt
-(=*) nm vals = let val = Txt.intercalate " " vals in nm =: val
+(=*) :: Txt -> [Txt] -> CSS Txt
+(=*) nm (Txt.intercalate " " -> val) = nm =: val
 
-comment :: Txt -> Styles Txt
-comment com = (=:) ("//" <> com) ""
+-- designed to maintain the old API
+important :: CSS a -> CSS a
+important (Return a) = Return a
+important (Lift l) = Lift (fmap important l)
+important (Do msg) =
+  case msg of
+    Raw_ r k -> Do (Raw_ r k)
+    Scope_ g -> Do (Scope_ (fmap important g))
+    Selection_ sel scoped ak -> Do (fmap important (Selection_ sel (important scoped) ak))
+    Wrap_ rule scoped ak -> Do (fmap important (Wrap_ rule (important scoped) ak))
+    Style_ k v a -> Do (fmap important (Style_ k (v <> " !important") a))
 
-classify :: Txt -> Txt
-classify = ("." <>) . toTxt
 
-important :: Styles a -> Styles a
-important s = buildn $ \r l d ->
-  let go (Style_ k v x) = d (Style_ k (v <> " !important") x)
-  in foldn r l go s
-
--- useful for debugging; keep the styles on the page, but block them with //
--- Can be used to comment out one or more styles
+-- | The core mechanism used to implement basic CSS selectors. See `is`, `has`,
+-- `next`, `child`.
 --
--- > ignore $ backgroundColor =: hideousOrange
+-- > select ".btn" do
+-- >   ...
 --
--- > ignore $ do
--- >   backgroundColor =: blue
--- >   fontSize =: ems 1.3
-ignore :: Styles a -> Styles a
-ignore s = buildn $ \r l d ->
-  let go (Style_ k v x) = d (Style_ ("// " <> k) v x)
-  in foldn r l go s
-
-renderStyles :: Bool -> Styles a -> ([Txt],a)
-renderStyles b = first Prelude.reverse . runIdentity . flip (thread go) []
-  where
-    go (Style_ k v r) acc =
-       r (((if b then "\t\t" else "\t") <> k <> ": " <> v):acc)
-
-styled :: Styles a -> (Features -> Features)
-styled = Styles . fst . runIdentity . flip (thread collect) mempty
-  where
-    collect (Style_ k v r) = r . ((k,v):)
-
-getStyles :: Features -> Styles ()
-getStyles = sequence_ . M.mapWithKey (=:) . styles
-
-data CSS_ k where
-  CSS_ :: Txt -> Styles a -> (a -> k) -> CSS_ k
-  CSS3_ :: Txt -> Txt -> CSS a -> (a -> k) -> CSS_ k
-  RawCSS_ :: Txt -> k -> CSS_ k
-
-instance Functor CSS_ where
-  fmap f (CSS_ t ss ak) = CSS_ t ss (fmap f ak)
-  fmap f (CSS3_ n l cs ak) = CSS3_ n l cs (fmap f ak)
-  fmap f (RawCSS_ c k) = RawCSS_ c (f k)
-
-type CSS = Narrative CSS_ Identity
-instance Default (CSS ()) where
-  def = return ()
-
-selector :: Txt -> Styles a -> CSS a
-selector sel ss = send (CSS_ sel ss id)
-
-apply :: Styles a -> CSS ()
-apply = void . selector ""
-
-apply' :: Styles a -> CSS a
-apply' = selector ""
-
-infixr 0 .>
-(.>) :: (CSS () -> CSS ()) -> Styles a -> CSS ()
-(.>) f decls = f $ apply decls
-
-infixr 0 ..>
-(..>) :: (CSS a -> CSS b) -> Styles a -> CSS b
-(..>) f decls = f $ apply' decls
-
-reusable :: Monad m => m a -> m (m a,a)
-reusable ma = do
-  a <- ma
-  return (ma,a)
-
-newtype Composable a = Composable { composes :: CSS a }
-composable :: CSS a -> CSS (Composable a)
-composable css = css >> return (Composable css)
-
-newtype Extendable a = Extendable { extends :: Styles a }
-extendable :: Styles a -> Styles (Extendable a)
-extendable ss = ss >> return (Extendable ss)
-
-rawCSS_ :: Txt -> CSS ()
-rawCSS_ c = Do (RawCSS_ c (Return ()))
-
+-- Produces:
+--
+-- > .btn {
+-- >   ...
+-- > }
+--
 select :: Txt -> CSS a -> CSS a
-select sel s = buildn $ \r l d ->
-  let go (CSS_ suf ss rest) =
-        d (CSS_ (sel <> suf) ss rest)
+select sel scoped = send (Selection_ sel scoped id)
 
-      go (CSS3_ at rule css rest) =
-        d (CSS3_ at rule (select sel css) rest)
+-- | `rescope` allows the implementation of @_ queries by re-wrapping
+-- the current scope at the root level, making it possible to
+-- wrap a scope block with a CSS3 selector:
+--
+-- > at @SomeClass do
+-- >   rescope "@media screen and (min-width: 400px)" do
+-- >     ...
+--
+-- Produces:
+--
+-- > @media screen and (min-width: 400px) {
+-- >   .SomeClass {
+-- >     ...
+-- >   }
+-- > }
+rescope :: Txt -> CSS a -> CSS a
+rescope rule scoped = send (Wrap_ rule scoped id)
 
-  in foldn r l go s
+{-# DEPRECATED apply, (.>), (..>) "apply, (.>), and (..>) are no longer necessary to introduce a styling scope as the styling scope and selection scopes have been merged." #-}
+apply :: CSS a -> CSS a
+apply = id
+infixr 0 .>
+(.>) = ($)
+infixr 0 ..>
+(..>) = ($)
+
+
+-- * Renderers
+
+stylesheet :: CSS a -> Txt
+stylesheet = start
+  where
+    start :: CSS x -> Txt
+    start (Return _) = ""
+    start (Lift i) = start (runIdentity i)
+    start (Do msg) =
+      case msg of
+        Raw_ r k -> r <> "\n" <> start k
+        
+        Scope_ f -> start (f "")
+
+        -- Couldn't figure a way to disallow this case; it is an artifact of 
+        -- the unification of the css and style DSLs.
+        Style_ _ _ k -> start k
+
+        Selection_ sel scoped k -> 
+          let (res,a) = selecting 0 sel "" "" scoped 
+           in res <> start (k a)
+        Wrap_ rule scoped k ->
+          let (res,a) = selecting 1 "" "" "" scoped
+           in rule <> " {\n" <> res <> "}\n\n" <> start (k a)
+
+
+    selecting :: Int -> Txt -> Txt -> Txt -> CSS a -> (Txt,a)
+    selecting depth sel acc rest (Return a) = 
+      case (Txt.null acc,Txt.null rest) of
+        (True,True) -> (mempty,a)
+        (True,_   ) -> (rest,a)
+        _           -> (Txt.replicate depth "\t" <> sel <> " {\n" <> acc <> Txt.replicate depth "\t" <> "}\n\n" <> rest,a)
+    selecting depth sel acc rest (Lift l) = selecting depth sel acc rest (runIdentity l)
+    selecting depth sel acc rest (Do msg) =
+      case msg of
+        Scope_ f -> selecting depth sel acc rest (f sel)
+        Raw_ r k -> selecting depth sel acc (rest <> r <> "\n") k
+        Style_ k v cont -> selecting depth sel (acc <> (Txt.replicate (depth + 1) "\t") <> k <> ": " <> v <> ";\n") rest cont
+        Selection_ sel' scoped k -> 
+          let (res,a) = selecting depth (sel <> sel') "" "" scoped
+           in selecting depth sel acc (rest <> res) (k a)
+        Wrap_ rule scoped k ->
+          let (res,a) = selecting (depth + 1) sel "" "" scoped
+           in selecting depth sel acc (rest <> Txt.replicate depth "\t" <> rule <> " {\n" <> res <> Txt.replicate depth "\t" <> "}\n\n" ) (k a)
+
+css :: CSS a -> View
+css = txt . stylesheet
 
 any :: Txt 
 any = "*"
@@ -241,12 +241,11 @@ is s c = void (select s c)
 is' :: Txt -> CSS a -> CSS a
 is' = select
 
--- equivalent to id; purely for scanning purposes
-and :: (Txt -> CSS a -> CSS a) -> Txt -> CSS a -> CSS a
-and f sel css = f sel css
+and :: (Txt -> CSS a -> CSS b) -> Txt -> CSS a -> CSS b
+and f = f
 
-or :: (Txt -> CSS a -> CSS a) -> Txt -> CSS a -> CSS a
-or f sel css = f (", " <> sel) css
+or :: (Txt -> CSS a -> CSS b) -> Txt -> CSS a -> CSS b
+or f sel = f (", " <> sel) 
 
 isn't :: Txt -> CSS a -> CSS a
 isn't sel = select (":not(" <> sel <> ")")
@@ -268,9 +267,6 @@ nthOfType i = select (":nth-of-type(" <> toTxt i <> ")")
 
 nthLastOfType :: Int -> CSS a -> CSS a
 nthLastOfType i = select (":nth-last-of-type(" <> toTxt i <> ")")
-
-compose :: (Category cat, Foldable t) => t (cat a a) -> cat a a
-compose = F.foldr (>>>) id
 
 use :: (Traversable t, Applicative f) => t (a -> f b) -> a -> f (t b)
 use fs x = for fs ($ x)
@@ -294,193 +290,38 @@ nexts :: Txt -> CSS a -> CSS a
 nexts sel = select (" ~ " <> sel)
 
 atCharset :: Txt -> CSS ()
-atCharset cs = send (CSS3_ "@charset " cs (return ()) id)
+atCharset cs = rawCSS ("@charset " <> cs)
 
 utf8Charset :: CSS ()
-utf8Charset = atCharset "UTF-8"
+utf8Charset = atCharset "\"UTF-8\";"
 
 iso885915Charset :: CSS ()
-iso885915Charset = atCharset "iso-8859-15"
+iso885915Charset = atCharset "\"iso-8859-15\";"
 
 atImport :: Txt -> CSS ()
-atImport i = send (CSS3_ "@import " i (Return ()) id)
+atImport i = rescope ("@import " <> i) (pure ())
 
 data Namespace = XHTMLNS | SVGNS
 
 atNamespace :: Namespace -> Maybe Txt -> CSS ()
-atNamespace ns mnsv = send (CSS3_ namespace_ ns_ (Return ()) id)
+atNamespace ns mnsv = rescope (namespace_ <> ns_) (pure ())
   where
     ns_ =
       case ns of
         XHTMLNS -> "url(http://www.w3.org/1999/xhtml)"
         SVGNS   -> "url(http://www.w3.org/2000/svg)"
 
-    namespace_ =
-      maybe "@namespace" ("@namespace " <>) mnsv
+    namespace_ = 
+      let n = "@namespace " in maybe n (n <>) mnsv
 
 atMedia :: Txt -> CSS a -> CSS a
-atMedia med c = send (CSS3_ "@media " med c id)
+atMedia med = rescope ("@media " <> med)
 
 atPage :: Txt -> CSS a -> CSS a
-atPage pgsel rls = send (CSS3_ "@page " pgsel rls id)
+atPage pg = rescope ("@page " <> pg)
 
 atFontFace :: Txt -> CSS a -> CSS a
-atFontFace ff rls = send (CSS3_ "@font-face " ff rls id)
-
-atWebkitKeyframes :: Txt -> CSS a -> CSS a
-atWebkitKeyframes nm kfs = send (CSS3_ "@-webkit-keyframes " nm kfs id)
+atFontFace ff = rescope ("@font-face " <> ff)
 
 atKeyframes :: Txt -> CSS a -> CSS a
-atKeyframes nm kfs = send (CSS3_ "@keyframes " nm kfs id)
-
--- data CSSError = InvalidCSSSyntax Txt deriving (Show)
--- instance Exception CSSError
-
-newtype StaticCSS = StaticCSS { cssText :: Txt } deriving (Eq,Ord,TH.Lift)
-instance ToTxt StaticCSS where
-  {-# INLINE toTxt #-}
-  toTxt (StaticCSS csst) = csst
-instance FromTxt StaticCSS where
-  {-# INLINE fromTxt #-}
-  fromTxt = StaticCSS
-instance Monoid StaticCSS where
-  mempty = fromTxt mempty
-#if !MIN_VERSION_base(4,11,0)
-  mappend csst1 csst2 = fromTxt $ toTxt csst1 <> "\n" <> toTxt csst2
-#else
-instance Semigroup StaticCSS where
-  (<>) csst1 csst2 = fromTxt $ toTxt csst1 <> "\n" <> toTxt csst2
-#endif
-mkRawCSS :: CSS () -> TH.Q TH.Exp
-mkRawCSS c = [| rawCSS_ $(TH.lift $ toTxt c) |]
-
-instance ToTxt (CSS a) where
-  toTxt = fst . go "\n" False
-    where
-      go acc b (Return a) = (acc,a)
-      go acc b (Lift s) = go acc b (runIdentity s)
-      go acc b (Do msg) =
-        case msg of
-          CSS3_ atRule sel css k ->
-            case css of
-              Return a ->
-                 go (acc <> atRule <> sel <> ";\n") False (k a)
-              _ ->
-                let (b,a) = go mempty True (unsafeCoerce css)
-                in go (acc <> atRule <> sel <> " {\n" <> b <> "\n}\n\n") False (unsafeCoerce k a)
-          CSS_ sel ss k ->
-            let (s,a) = renderStyles b (unsafeCoerce ss)
-                t = sel <> " {\n" <> Txt.intercalate ";\n" s <> if b then "\n\t}\n\n" else "\n}\n\n"
-            in go (acc <> if b then "\t" <> t else t) b (unsafeCoerce k a)
-          RawCSS_ css k ->
-            go (acc <> css <> "\n") b k
-
-staticCSS :: CSS a -> StaticCSS
-staticCSS = fromTxt . toTxt
-
-unindent :: Txt -> Txt
-unindent =
-  Txt.concat .
-  removeIndentation .
-  trimLastLine .
-  removeLeadingEmptyLine .
-  lines_
-  where
-    isEmptyLine :: Txt -> Bool
-    isEmptyLine = Txt.all isSpace
-
-    lines_ :: Txt -> [Txt]
-    lines_ s =
-      if Txt.null s
-      then []
-      else
-        case Txt.span (/= '\n') s of
-          (first, rest) ->
-            case Txt.uncons rest of
-              Just ('\n', more) -> (first <> Txt.pack "\n") : lines_ rest
-              _ -> first : lines_ rest
-
-    removeLeadingEmptyLine :: [Txt] -> [Txt]
-    removeLeadingEmptyLine xs = case xs of
-      y:ys | isEmptyLine y -> ys
-      _ -> xs
-
-    trimLastLine :: [Txt] -> [Txt]
-    trimLastLine (a : b : r) = a : trimLastLine (b : r)
-    trimLastLine [a] = if Txt.all (== ' ') a
-      then []
-      else [a]
-    trimLastLine [] = []
-
-    removeIndentation :: [Txt] -> [Txt]
-    removeIndentation ys = List.map (dropSpaces indentation) ys
-      where
-        dropSpaces 0 s = s
-        dropSpaces n s =
-          case Txt.uncons s of
-            Just (' ',r) -> dropSpaces (n - 1) r
-            _ -> s
-        indentation = minimalIndentation ys
-        minimalIndentation =
-            safeMinimum 0
-          . List.map (Txt.length . Txt.takeWhile (== ' '))
-          . removeEmptyLines
-        removeEmptyLines = List.filter (not . isEmptyLine)
-
-        safeMinimum :: Ord a => a -> [a] -> a
-        safeMinimum x xs = case xs of
-          [] -> x
-          _ -> List.minimum xs
-
-css :: Narrative CSS_ Identity a -> View
-css = css' False
-
-css' :: forall a e. Bool -> Narrative CSS_ Identity a -> View
-css' b nar = SimpleHTML "style" <| Property "type" "text/css" . Property "scoped" (if b then "true" else "") |> ("\n" : (fst $ go False [] nar))
-  where
-    go :: forall a. Bool -> [View] -> Narrative CSS_ Identity a -> ([View],a)
-    go b acc (Return a) = (acc,a)
-    go b acc (Lift s) = go b acc (runIdentity s)
-    go b acc c@(Do msg) =
-      case msg of
-        CSS3_ atRule sel css k ->
-          case css of
-            Return a ->
-              go False (acc ++ [ txt (atRule <> sel <> ";\n") ]) (k a)
-            _ ->
-              let (c,a) = go True [] css
-              in go False (acc ++ ( txt (atRule <> sel <> " {\n") : c) ++ [ "\n}\n\n" ]) (k a)
-        CSS_ sel ss r ->
-          let (s,a) = renderStyles b ss
-          in
-            go b  ( acc ++ [ txt ( (if b then "\t" else mempty)
-                                      <> sel
-                                      <> " {\n"
-                                      <> (Txt.intercalate (if b then ";\n\t" else ";\n") s)
-                                      <> (if b then "\n\t}\n\n" else "\n}\n\n")
-                                  )
-                           ]
-                  ) (r a)
-        RawCSS_ css k -> go b (acc ++ [ txt (css <> "\n") ]) k
-
-scss :: StaticCSS -> View
-scss = scss' False
-
-scss' :: Bool -> StaticCSS -> View
-scss' b = RawView Nothing "style" def { properties = M.fromList [("type","text/css"),("scoped",if b then "true" else "")] } . cssText
-
-inlineCSS :: Narrative CSS_ Identity a -> View
-inlineCSS = css' True . classify
-  where
-    classify :: forall a. Narrative CSS_ Identity a -> Narrative CSS_ Identity a
-    classify (Return r) = Return r
-    classify (Lift sup) = Lift (fmap classify sup)
-    classify (Do e) =
-      case e of
-        CSS_ sel ss k ->
-          Do (CSS_ (Txt.cons '.' sel) ss (classify . k))
-        CSS3_ at sel css k ->
-          Do (CSS3_ at sel (classify css) (classify . k))
-        RawCSS_ css k ->
-          Do (RawCSS_ css (classify k))
-
+atKeyframes nm = rescope ("@keyframes " <> nm)
